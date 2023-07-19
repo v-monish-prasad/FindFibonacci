0e7245400207622c449693febc98d0ee0c5e1df9
Monish Prasad V <82965969+v-monish-prasad@users.noreply.github.com>
2023-07-19 22:25:32 +0530

    Update FindFibonacci.py

--- FindFibonacci.py
+++ FindFibonacci.py
@@ -9,32 +9,3 @@ if __name__ == "__main__":
     number = int(input())
 
     print(findFibonacci(number))
-
-
-# The Fibonacci numbers are the numbers in the following integer sequence.
-#
-# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
-#
-# In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation:
-#
-# Fn = Fn-1 + Fn-2
-#
-# Given a number A, find and return the Ath Fibonacci Number using recursion.
-#
-# Given that F0 = 0 and F1 = 1.
-#
-# Example Input
-#
-# Input 1:
-# A = 2
-#
-# Input 2:
-# A = 9
-#
-# Example Output
-#
-# Output 1:
-# 1
-#
-# Output 2:
-# 34
